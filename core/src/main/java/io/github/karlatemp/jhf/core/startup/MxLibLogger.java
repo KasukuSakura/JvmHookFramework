c189497e4b8d11ab84c12d1dde1fcb255be10780
Karlatemp <karlatemp@vip.qq.com>
2021-06-27 11:36:10 +0800

    Fix logging options not applied

--- core/src/main/java/io/github/karlatemp/jhf/core/startup/MxLibLogger.java
+++ core/src/main/java/io/github/karlatemp/jhf/core/startup/MxLibLogger.java
@@ -1,6 +1,7 @@
 package io.github.karlatemp.jhf.core.startup;
 
 import io.github.karlatemp.jhf.core.config.JHFConfig;
+import io.github.karlatemp.jhf.core.utils.IOUtils;
 import io.github.karlatemp.mxlib.MxLib;
 import io.github.karlatemp.mxlib.logger.*;
 import io.github.karlatemp.mxlib.logger.renders.PrefixSupplierBuilder;
@@ -12,6 +13,7 @@ import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.Property;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -23,15 +25,36 @@ public class MxLibLogger extends AbstractAppender {
     private static final MessageRender render;
     private static final Map<Level, java.util.logging.Level> mappings = new HashMap<>();
 
+    private static class LogX extends AwesomeLogger.Awesome.Awesome {
+        public LogX(String name, Consumer<StringBuilder> printer, MessageRender render) {
+            super(name, printer, render);
+        }
+
+        @Override
+        public boolean isEnabled(java.util.logging.Level level) {
+            java.util.logging.Level CONFL = JHFConfig.INSTANCE.loggingLevel;
+            if (CONFL != null) return level.intValue() >= CONFL.intValue();
+            return true;
+        }
+    }
+
     static {
         MessageFactory bmf = JHFConfig.INSTANCE.useAnsiMessage
                 ? new AnsiMessageFactory()
                 : new RawMessageFactory();
 
-        PrefixedRender.PrefixSupplier result = PrefixSupplierBuilder.parseFrom(
-                new Scanner(Objects.requireNonNull(
+        String prefixConf = new String(
+                IOUtils.readAndClose(Objects.requireNonNull(
                         MxLibLogger.class.getResourceAsStream("/jhf/logger/prefix.txt")
-                ))
+                )),
+                StandardCharsets.UTF_8
+        );
+        if (!JHFConfig.INSTANCE.useAnsiMessage) {
+            prefixConf = prefixConf.replaceAll("\\u00a7.", "");
+        }
+
+        PrefixedRender.PrefixSupplier result = PrefixSupplierBuilder.parseFrom(
+                new Scanner(prefixConf)
         );
         PrefixedRender renderX = new PrefixedRender(
                 new SimpleRender(bmf), result
@@ -47,7 +70,7 @@ public class MxLibLogger extends AbstractAppender {
         MxLib.setLoggerFactory(new MLoggerFactory() {
             @Override
             public @NotNull MLogger getLogger(String name) {
-                return new AwesomeLogger.Awesome.Awesome(
+                return new LogX(
                         name,
                         printer,
                         render
@@ -63,7 +86,7 @@ public class MxLibLogger extends AbstractAppender {
 
     @Override
     public void append(LogEvent event) {
-        MLogger logger = new AwesomeLogger.Awesome.Awesome(
+        MLogger logger = new LogX(
                 event.getLoggerName(),
                 printer,
                 render
