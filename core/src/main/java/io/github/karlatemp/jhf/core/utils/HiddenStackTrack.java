3bab8711d6e1c9d6d1c44e74e7ae66d13fc46b36
Karlatemp <karlatemp@vip.qq.com>
2021-06-27 11:36:29 +0800

    Improve `HiddenStackTrack`

--- core/src/main/java/io/github/karlatemp/jhf/core/utils/HiddenStackTrack.java
+++ core/src/main/java/io/github/karlatemp/jhf/core/utils/HiddenStackTrack.java
@@ -7,11 +7,13 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class HiddenStackTrack {
+    public static final String EVENT_PIPELINE_CLASS_NAME = "io.github.karlatemp.jhf.api.event.EventLine";
     public static ArrayList<Predicate<StackTraceElement>> shouldHidden = new ArrayList<>();
 
     static {
         shouldHidden.add(t -> t.getClassName().startsWith("io.github.karlatemp.jhf."));
         shouldHidden.add(t -> t.getClassName().startsWith("java.lang.ReflectionLimit$B$$"));
+        shouldHidden.add(t -> t.getClassName().startsWith("java.lang.JLReflectInvokeRfRf$Z$$"));
     }
 
     public static void hidden(Throwable throwable) {
@@ -31,20 +33,42 @@ public class HiddenStackTrack {
         return false;
     }
 
+
     private static void hiddenUp(Throwable throwable) {
         if (throwable == null) return;
+        if (JHFConfig.INSTANCE.hiddenAll) {
+            hidden1(throwable);
+        } else {
+            hidden0(throwable);
+        }
+    }
+
+    private static void hidden0(Throwable throwable) {
         StackTraceElement[] stackTrace = throwable.getStackTrace();
         int ed = stackTrace.length;
-        for (int i = 0; i < ed; i++) {
+        for (int i = ed - 1; i >= 0; i--) {
             if (doHidden(stackTrace[i])) {
                 ArrayList<StackTraceElement> stackTraceElements = new ArrayList<>(stackTrace.length);
-                stackTraceElements.addAll(Arrays.asList(stackTrace).subList(0, i));
-                for (int z = i + 1; z < ed; z++) {
+                stackTraceElements.addAll(Arrays.asList(stackTrace).subList(i + 1, ed));
+                for (int z = i; z >= 0; z--) {
+                    if (stackTrace[z].getClassName().equals(EVENT_PIPELINE_CLASS_NAME))
+                        break;
                     if (!doHidden(stackTrace[z])) {
-                        stackTraceElements.add(stackTrace[z]);
+                        stackTraceElements.add(0, stackTrace[z]);
                     }
                 }
                 throwable.setStackTrace(stackTraceElements.toArray(new StackTraceElement[0]));
+                return;
+            }
+        }
+    }
+
+    private static void hidden1(Throwable throwable) {
+        StackTraceElement[] stackTrace = throwable.getStackTrace();
+        int ed = stackTrace.length;
+        for (int i = ed - 1; i >= 0; i--) {
+            if (doHidden(stackTrace[i])) {
+                throwable.setStackTrace(Arrays.copyOfRange(stackTrace, i + 1, ed));
                 return;
             }
         }
