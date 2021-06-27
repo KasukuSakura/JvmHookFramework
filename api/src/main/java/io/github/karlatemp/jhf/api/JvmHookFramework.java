b6bfe8cd9b292dbe763ed5d071607b0ac49705a9
Karlatemp <karlatemp@vip.qq.com>
2021-06-27 11:36:46 +0800

    Update public bridges

--- api/src/main/java/io/github/karlatemp/jhf/api/JvmHookFramework.java
+++ api/src/main/java/io/github/karlatemp/jhf/api/JvmHookFramework.java
@@ -2,6 +2,12 @@ package io.github.karlatemp.jhf.api;
 
 import io.github.karlatemp.jhf.api.event.EventHandler;
 import io.github.karlatemp.jhf.api.event.EventLine;
+import org.spongepowered.configurate.ConfigurationOptions;
+import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+import org.spongepowered.configurate.serialize.TypeSerializerCollection;
+
+import java.io.File;
+import java.nio.file.Path;
 
 public abstract class JvmHookFramework {
     private static JvmHookFramework INSTANCE;
@@ -18,4 +24,14 @@ public abstract class JvmHookFramework {
             T event,
             Exception exception
     );
+
+    public abstract File getWorkingDir();
+
+    public File getDataFolder(String name) {
+        return new File(getWorkingDir(), "data/" + name);
+    }
+
+    public abstract HoconConfigurationLoader newConfigLoader(ConfigurationOptions options, Path path);
+
+    public abstract TypeSerializerCollection jhfExtractTypeSerializers();
 }
--- core/src/main/java/io/github/karlatemp/jhf/core/startup/JHFrameworkImpl.java
+++ core/src/main/java/io/github/karlatemp/jhf/core/startup/JHFrameworkImpl.java
@@ -3,10 +3,17 @@ package io.github.karlatemp.jhf.core.startup;
 import io.github.karlatemp.jhf.api.JvmHookFramework;
 import io.github.karlatemp.jhf.api.event.EventHandler;
 import io.github.karlatemp.jhf.api.event.EventLine;
+import io.github.karlatemp.jhf.core.config.JHFConfig;
 import io.github.karlatemp.jhf.core.utils.HiddenStackTrack;
 import io.github.karlatemp.mxlib.MxLib;
 import io.github.karlatemp.mxlib.logger.MLogger;
 import io.github.karlatemp.mxlib.utils.StringBuilderFormattable;
+import org.spongepowered.configurate.ConfigurationOptions;
+import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+import org.spongepowered.configurate.serialize.TypeSerializerCollection;
+
+import java.io.File;
+import java.nio.file.Path;
 
 class JHFrameworkImpl extends JvmHookFramework {
     private static final MLogger EVENT_LOGGER = MxLib.getLoggerFactory().getLogger("JHF.Event");
@@ -28,5 +35,20 @@ class JHFrameworkImpl extends JvmHookFramework {
                         .plusMsg("]"),
                 exception
         );
+    }
+
+    @Override
+    public File getWorkingDir() {
+        return JHFConfig.workingDir;
+    }
+
+    @Override
+    public HoconConfigurationLoader newConfigLoader(ConfigurationOptions options, Path path) {
+        return JHFConfig.newLoader(options, path);
+    }
+
+    @Override
+    public TypeSerializerCollection jhfExtractTypeSerializers() {
+        return JHFConfig.EXTRACT_SERIALIZERS;
     }
 }
