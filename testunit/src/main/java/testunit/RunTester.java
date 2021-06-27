d13832cb0b1c478a130bc570cdf1b21564cfb67f
Karlatemp <karlatemp@vip.qq.com>
2021-06-27 11:34:26 +0800

    Update test unit

--- testunit/src/main/java/testunit/RunTester.java
+++ testunit/src/main/java/testunit/RunTester.java
@@ -14,11 +14,13 @@ public class RunTester {
                     .invoke(null);
         } catch (IllegalAccessException e) {
             Assertions.assertEquals("Permission Denied", e.getMessage());
+            e.printStackTrace(System.out);
         }
         try {
             EskPermissionDenied.class.getMethod("notFound");
             throw new AssertionError();
-        } catch (NoSuchMethodException ignore) {
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace(System.out);
         }
 
     }
