17110a8bc902ab4692352143f498488737cbca40
Karlatemp <karlatemp@vip.qq.com>
2021-07-06 23:48:47 +0800

    Remove tester for redesigning

--- testunit/src/main/java/testunit/RunTester.java
+++ testunit/src/main/java/testunit/RunTester.java
@@ -1,7 +1,5 @@
 package testunit;
 
-import org.junit.jupiter.api.Assertions;
-
 public class RunTester {
     public static void error() {
         throw new AssertionError();
@@ -9,19 +7,5 @@ public class RunTester {
 
     public static void main(String[] args) throws Throwable {
         error();
-        try {
-            EskPermissionDenied.class.getMethod("doPermissionDenied")
-                    .invoke(null);
-        } catch (IllegalAccessException e) {
-            Assertions.assertEquals("Permission Denied", e.getMessage());
-            e.printStackTrace(System.out);
-        }
-        try {
-            EskPermissionDenied.class.getMethod("notFound");
-            throw new AssertionError();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace(System.out);
-        }
-
     }
 }
